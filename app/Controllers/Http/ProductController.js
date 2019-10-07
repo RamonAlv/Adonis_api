e065e4a26eb8889bd597caed56ad044af18a5f2c
Ramon Alvarado <171106@ids.upchiapas.edu.mx>
2019-10-07 00:12:05 -0500

    img correccion

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -12,6 +12,8 @@ const Product = use('App/Models/Product');
 const Inventorie = use('App/Models/Inventorie')
 const Transaction = use('App/Models/Transaction')
 
+const Helpers = use('Helpers')
+
 class ProductController {
   /**
    * Show a list of all products.
@@ -64,7 +66,15 @@ class ProductController {
         product.code = data.code
         product.name = data.name
         product.description = data.description
-        product.image = data.image
+        //img upload
+        const IMG = request.file('image')
+
+        product.image = data.code+'-'+data.name+'.'+IMG.subtype
+
+        await IMG.move(Helpers.publicPath('uploads/'+user.id), {
+          name: product.image
+        })
+
         await product.save();
         //Inventory
         inventory.product_id = product.id
